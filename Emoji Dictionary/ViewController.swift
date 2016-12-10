674f257b22be7c3d21c14ce3bdb4e0b8b84e92ab
Napatsorn Lam Sakulsuwan <PATCHUNG@Napatsorns-MacBook-Air.local>
2016-12-11 02:16:47 +0700

    learn about table view and added array + Second VC

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource ,UITableViewDelegate {
+    
+    @IBOutlet weak var tableview: UITableView!
+    
+    var candyBar = ["Pink","Red","Blue", "Black"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableview.dataSource = self
+        tableview.delegate = self
+        
     }
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return candyBar.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = candyBar[indexPath.row]
+        return cell
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
